853917131ee5d34d5b9c90cff7be518ff6d49425
Fland Pan <43575097+FlandPan@users.noreply.github.com>
2020-08-02 11:14:25 -0400

    Needs some fixing

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Scene0(){
+        SceneManager.LoadScene(0);
+    }
+    public void Scene1(){
+        SceneManager.LoadScene(1);
+    }
+}
